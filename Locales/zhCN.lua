2ecfeb045239556e84becc678eae03f1f65743a8
KittenBall <912967642@qq.com>
2024-05-06 10:45:57 +0800

    Update zhCN.lua

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local addonName, Addon = ...
 
-local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "zhCN", true)
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "zhCN")
 
 if not L then return end
 
